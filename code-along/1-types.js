d0d3eb3ca61f3290e0f217728c40d607cf6d09c6
Sienna Kim <sienna.kim@kellogg.northwestern.edu>
2021-04-13 11:21:34 -0500

    After class

--- code-along/1-types.js
+++ code-along/1-types.js
@@ -2,9 +2,13 @@
 // Write the algorithm, in the comments. Write the code.
 
 // Store the item I want to eat
-
+let food = `tacos`
 // Store the quantity I want
+let quantity = 3
 
-// Make an English sentence
+// Make an English sentence, e.g. "I would like 3 tacos, please." This is a human readable data. step by step process of creating finish products.
+
+let sentence = `I would like ${quantity} ${food}, please.`
 
 // Write the sentence to the console
+console.log(sentence)
--- code-along/2-conditionals.js
+++ code-along/2-conditionals.js
@@ -18,3 +18,36 @@ function getRandomInt(max) {
 let input = prompt(`Enter a number between 1 and 10`)
 
 // Write the recipe (algorithm) in the comments. Then, write the code.
+
+// store reandom integer in memory
+let randomNumber =getRandomInt(10)
+//give us random number of 1~10
+// Javascript convention strat with lowercase letter and separate by using captial
+
+
+// show the randome integer in the console
+console.log(`The random number is ${randomNumber}`)
+
+//show the user's number
+console.log(`You entered ${input}`)
+
+// If the user did not enter a number betwee 1-10, display an error
+if (input <1) {
+  console.log(`You're really bad at following instructions...`)
+}
+else if (input > 10) {
+  console.log(`You're really abad at follwoing instructions....`)
+}
+//or we could you if (input <1 || input>10 { console.log...})
+
+// comparison operator is double equal signs
+// If the user guessed the number, display "YOU WIN!"
+else if (input == randomNumber) {
+  console.log(`YOU WIN!`)
+}
+
+
+// If the user did not guess the number, display "SORRY!"
+else {
+  console.log(`SORRY....`)
+}
